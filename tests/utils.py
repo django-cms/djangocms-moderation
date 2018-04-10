e4703da4c50d3bc37577d8e4e1a93b2ef1c3ec29
Noel da Costa <noel@arc2.co.uk>
2018-04-10 10:20:34 +0100

    Updated the Unittests to cater for model changes.

--- tests/utils.py
+++ tests/utils.py
@@ -14,7 +14,7 @@ class BaseTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         # create workflows
-        cls.wf1 = Workflow.objects.create(pk=1, name='Workflow 1', is_default=True)
+        cls.wf1 = Workflow.objects.create(pk=1, name='Workflow 1', is_default=True, is_reference_number_required=True, reference_number_prefix="")
         cls.wf2 = Workflow.objects.create(pk=2, name='Workflow 2')
 
         # create pages
@@ -37,13 +37,13 @@ class BaseTestCase(TestCase):
         WorkflowStep.objects.create(role=cls.role3, is_required=True, workflow=cls.wf2, order=2)
 
         # create page moderation request and action
-        cls.moderation_request1 = PageModerationRequest.objects.create(page=cls.pg1, language='en', workflow=cls.wf1, is_active=True)
+        cls.moderation_request1 = PageModerationRequest.objects.create(page=cls.pg1, language='en', workflow=cls.wf1, is_active=True, reference_number="00000000000001")
         cls.moderation_request1.actions.create(by_user=cls.user, to_user=cls.user, action=constants.ACTION_STARTED)
 
-        PageModerationRequest.objects.create(page=cls.pg1, language='en', workflow=cls.wf1, is_active=False)
-        PageModerationRequest.objects.create(page=cls.pg2, language='en', workflow=cls.wf2, is_active=False)
+        PageModerationRequest.objects.create(page=cls.pg1, language='en', workflow=cls.wf1, is_active=False, reference_number="00000000000002")
+        PageModerationRequest.objects.create(page=cls.pg2, language='en', workflow=cls.wf2, is_active=False, reference_number="00000000000003")
 
-        cls.moderation_request2 = PageModerationRequest.objects.create(page=cls.pg3, language='en', workflow=cls.wf2, is_active=True)
+        cls.moderation_request2 = PageModerationRequest.objects.create(page=cls.pg3, language='en', workflow=cls.wf2, is_active=True, reference_number="00000000000004")
         cls.moderation_request2.actions.create(by_user=cls.user, to_user=cls.user, action=constants.ACTION_STARTED)
         cls.moderation_request2.actions.create(by_user=cls.user, to_user=cls.user, action=constants.ACTION_APPROVED, step_approved=WorkflowStep.objects.get(workflow=cls.wf2, order=1))
         cls.moderation_request2.actions.create(by_user=cls.user, to_user=cls.user, action=constants.ACTION_APPROVED, step_approved=WorkflowStep.objects.get(workflow=cls.wf2, order=2))
