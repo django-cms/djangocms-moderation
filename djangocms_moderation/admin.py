1c3f0b94cc65310d74601248789c2d4f43db597d
Noel da Costa <noel@arc2.co.uk>
2018-09-18 15:04:15 +0100

    Refactored as the merge broke a few things
    - change_view wasn't passing context for some reason
    - admin_modify.py was risky as it touches all admin pages and we were really just interested in modifying the comments admin, so it has been removed
    - comments_extras.py had some scoping issues and also some isort issues. The code from admin_modify was merged into comments_extras

--- djangocms_moderation/admin.py
+++ djangocms_moderation/admin.py
@@ -308,26 +308,23 @@ class CollectionCommentAdmin(admin.ModelAdmin):
             raise Http404
         return super().changelist_view(request, extra_context)
 
-    def change_view(self, request, object_id, form_url='', extra_context=None):
-        extra_context = extra_context or {}
-
-        # hide these buttons as they are not relevant for comments
-        extra_context['show_save_and_add_another'] = False
-        extra_context['show_save_and_continue'] = False
-
-        collection_comment = get_object_or_404(CollectionComment, pk=int(object_id))
-        if request.user != collection_comment.author:
-            extra_context['readonly'] = True
-
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
         # get the collection for the breadcrumb trail
         collection_id = utils.extract_filter_param_from_changelist_url(
             request, '_changelist_filters', 'collection__id__exact'
         )
+        extra_context = extra_context or dict(
+            show_save_and_add_another=False,
+            show_save_and_continue=False,
+        )
+        if object_id:
+            collection_comment = get_object_or_404(CollectionComment, pk=int(object_id))
+            if request.user != collection_comment.author:
+                extra_context['readonly'] = True
+
         if collection_id:
             extra_context['collection_id'] = collection_id
-
-        return super().change_view(request, object_id,
-                                   form_url, extra_context=extra_context)
+        return super().changeform_view(request, object_id, form_url, extra_context)
 
     def has_delete_permission(self, request, obj=None):
         return request.user == getattr(obj, 'author', None)
@@ -396,16 +393,15 @@ class RequestCommentAdmin(admin.ModelAdmin):
             raise Http404
         return super().changelist_view(request, extra_context)
 
-    def change_view(self, request, object_id, form_url='', extra_context=None):
-        extra_context = extra_context or {}
-
-        # hide these buttons as they are not relevant for comments
-        extra_context['show_save_and_add_another'] = False
-        extra_context['show_save_and_continue'] = False
-
-        request_comment = get_object_or_404(RequestComment, pk=int(object_id))
-        if request.user != request_comment.author:
-            extra_context['readonly'] = True
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or dict(
+            show_save_and_add_another=False,
+            show_save_and_continue=False,
+        )
+        if object_id:
+            request_comment = get_object_or_404(RequestComment, pk=int(object_id))
+            if request.user != request_comment.author:
+                extra_context['readonly'] = True
 
         # for breadcrumb trail
         moderation_request_id = utils.extract_filter_param_from_changelist_url(
@@ -414,8 +410,7 @@ class RequestCommentAdmin(admin.ModelAdmin):
         if moderation_request_id:
             extra_context['moderation_request_id'] = moderation_request_id
 
-        return super().change_view(request, object_id,
-                                   form_url, extra_context=extra_context)
+        return super().changeform_view(request, object_id, form_url, extra_context)
 
     def has_delete_permission(self, request, obj=None):
         return request.user == getattr(obj, 'author', None)
