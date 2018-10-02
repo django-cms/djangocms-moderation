9ebe7ccfb1b773e6ed72333cc284460e8d6d1d1b
Krzysztof Socha <k.socha@merixstudio.com>
2018-10-02 18:34:20 +0200

    More splitting

--- djangocms_moderation/migrations/0006_auto_20181001_1840.py
+++ djangocms_moderation/migrations/0006_auto_20181001_1840.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='moderationrequest',
             name='author',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, related_name='+', to=settings.AUTH_USER_MODEL, verbose_name='author'),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='+', to=settings.AUTH_USER_MODEL, verbose_name='author'),
             preserve_default=False,
         ),
     ]
