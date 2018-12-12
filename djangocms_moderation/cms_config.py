d1e5cb72a122c2c7dfa5ce792ea1fa66cd3f5942
Krzysztof Socha <k.socha@merixstudio.com>
2018-12-12 10:42:22 +0100

    lint

--- djangocms_moderation/cms_config.py
+++ djangocms_moderation/cms_config.py
@@ -1,4 +1,3 @@
-from django.apps import apps
 from django.core.exceptions import ImproperlyConfigured
 
 from cms.app_base import CMSAppConfig, CMSAppExtension
@@ -16,15 +15,6 @@ class ModerationExtension(CMSAppExtension):
 
         if not versioning_enabled:
             raise ImproperlyConfigured('Versioning needs to be enabled for Moderation')
-
-        # versioning_extension = apps.get_app_config('djangocms_versioning').cms_extension
-        # for model in moderated_models:
-        #     # @todo replace this with a to be provided func from versioning_extensions
-        #     if model not in versioning_extension.versionables_by_content:
-        #         raise ImproperlyConfigured(
-        #             'Moderated model %s need to be Versionable, please include every model that '
-        #             'needs to be moderated in djangocms_versioning VersionableItem entry' % model
-        #         )
 
         self.moderated_models.extend(moderated_models)
 
