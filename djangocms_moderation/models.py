51134c64f2f9945042334a79a413d394122f7343
Aiky30 <aiky30@yahoo.com>
2019-02-04 11:49:33 +0000

    Removed unused import in Models

--- djangocms_moderation/models.py
+++ djangocms_moderation/models.py
@@ -5,7 +5,6 @@ import json
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
-from django.contrib.sites.models import Site
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
 from django.db import models, transaction
