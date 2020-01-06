2bf0d575e44c6d59eaa1fe4883defed9428e0d21
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-01-06 16:15:03 +0100

    Use https for dependencies

--- setup.py
+++ setup.py
@@ -49,9 +49,9 @@ setup(
     tests_require=TEST_REQUIREMENTS,
     test_suite="tests.settings.run",
     dependency_links=[
-        "http://github.com/divio/django-cms/tarball/release/4.0.x#egg=django-cms-4.0.0",
-        "http://github.com/divio/djangocms-versioning/tarball/master#egg=djangocms-versioning-0.0.23",
-        "http://github.com/FidelityInternational/djangocms-version-locking/tarball/master#egg=djangocms-version-locking-0.0.13", # noqa
+        "https://github.com/divio/django-cms/tarball/release/4.0.x#egg=django-cms-4.0.0",
+        "https://github.com/divio/djangocms-versioning/tarball/master#egg=djangocms-versioning-0.0.23",
+        "https://github.com/FidelityInternational/djangocms-version-locking/tarball/master#egg=djangocms-version-locking-0.0.13", # noqa
         "https://github.com/divio/djangocms-text-ckeditor/tarball/support/4.0.x#egg=djangocms-text-ckeditor-4.0.x"
     ]
 )
