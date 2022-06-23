0e26170cf8a7ab91d2306acd9f412032924dde9a
Adam Murray <adam-murray@hotmail.co.uk>
2022-06-23 11:21:10 +0100

    fix: Frontend linting

--- djangocms_moderation/static/djangocms_moderation/js/burger.js
+++ djangocms_moderation/static/djangocms_moderation/js/burger.js
@@ -6,7 +6,7 @@
     $(function () {
       // INFO: it is not possible to put a form inside a form, so the moderation actions have to create their own form
       // on click.
-      $(` .cms-moderation-action-btn,
+        $(` .cms-moderation-action-btn,
             .js-moderation-action,
             .cms-actions-dropdown-menu-item-anchor`)
       .on('click', function (e) {
@@ -28,39 +28,39 @@
 
           // always break out of the sideframe, cause it was never meant to open cms views inside it
           try {
-            if (!keepSideFrame) {
-              window.top.CMS.API.Sideframe.close();
-            }
-          } catch (err) { }
+              if (!keepSideFrame) {
+                window.top.CMS.API.Sideframe.close();
+              }
+            } catch (err) { }
           if (keepSideFrame) {
-            body = window.document.body;
-          }
+              body = window.document.body;
+            }
 
           fakeForm.appendTo(body).submit();
-        });
-
-      $('.js-versioning-close-sideframe').on('click', function () {
-        try {
-          window.top.CMS.API.Sideframe.close();
-        } catch (e) { }
       });
+
+        $('.js-versioning-close-sideframe').on('click', function () {
+          try {
+            window.top.CMS.API.Sideframe.close();
+          } catch (e) { }
+        });
     });
 
     // Hide django messages after timeout occurs to prevent content overlap
     $('document').ready(function () {
       // Targeting first item returned (there's only ever one messagelist per template):
-      let messageList = document.getElementsByClassName('messagelist')[0];
+        let messageList = document.getElementsByClassName('messagelist')[0];
 
-      if (messageList !== undefined) {
-        for (let item of messageList.children) {
-          item.style.opacity = 1;
-          setTimeout(() => {
-            let fader = setInterval(() => {
-              item.style.opacity -= 0.05;
-              if (item.style.opacity < 0) {
-                item.style.display = 'none';
-                clearInterval(fader);
-              }
+        if (messageList !== undefined) {
+          for (let item of messageList.children) {
+            item.style.opacity = 1;
+            setTimeout(() => {
+              let fader = setInterval(() => {
+                item.style.opacity -= 0.05;
+                if (item.style.opacity < 0) {
+                  item.style.display = 'none';
+                  clearInterval(fader);
+                }
 
             }, 20);
           }, 5000);
@@ -71,117 +71,116 @@
 
     // Create burger menu:
     $(function () {
-      let burger_menu_icon;
+        let burger_menu_icon;
 
-      burger_menu_icon = '/static/djangocms_versioning/svg/menu.svg';
+        burger_menu_icon = '/static/djangocms_versioning/svg/menu.svg';
 
-      let createBurgerMenu = function createBurgerMenu(row) {
+        let createBurgerMenu = function createBurgerMenu(row) {
 
-        let actions = $(row).children('.field-list_display_actions');
+          let actions = $(row).children('.field-list_display_actions');
 
-        if (!actions.length) {
-          /* skip any rows without actions to avoid errors */
-          return;
-        }
-
-        /* create burger menu anchor icon */
-        let anchor = document.createElement('a');
-        let icon = document.createElement('img');
-
-        icon.setAttribute('src', burger_menu_icon);
-        anchor.setAttribute('class', 'btn cms-moderation-action-btn closed');
-        anchor.setAttribute('title', 'Actions');
-        anchor.appendChild(icon);
-
-        /* create options container */
-        let optionsContainer = document.createElement('div');
-        let ul = document.createElement('ul');
-
-        /* 'cms-actions-dropdown-menu' class is the main selector for the menu,
-        'cms-actions-dropdown-menu-arrow-right-top' keeps the menu arrow in position. */
-        optionsContainer.setAttribute(
-          'class',
-          'cms-actions-dropdown-menu cms-actions-dropdown-menu-arrow-right-top');
-        ul.setAttribute('class', 'cms-actions-dropdown-menu-inner');
-
-        /* get the existing actions and move them into the options container */
-        $(actions[0]).children('.cms-moderation-action-btn').each(function (index, item) {
-
-          let li = document.createElement('li');
-          /* create an anchor from the item */
-          let li_anchor = document.createElement('a');
-
-          li_anchor.setAttribute('class', 'cms-actions-dropdown-menu-item-anchor');
-          li_anchor.setAttribute('href', $(item).attr('href'));
-
-          if ($(item).hasClass('cms-form-get-method')) {
-            li_anchor.classList.add('cms-form-get-method'); // Ensure the fake-form selector is propagated to the new anchor
+          if (!actions.length) {
+            /* skip any rows without actions to avoid errors */
+            return;
           }
-          /* get the span which contains the img */
-          let value = $(item).children('span')[0]
-          /* move the icon image */
-          li_anchor.appendChild($(value).children('img')[0]);
 
-          /* create the button text and construct the button */
-          let span = document.createElement('span');
-          span.appendChild(
-            document.createTextNode(item.title)
-          );
+          /* create burger menu anchor icon */
+          let anchor = document.createElement('a');
+          let icon = document.createElement('img');
 
-          li_anchor.appendChild(span);
-          li.appendChild(li_anchor);
-          ul.appendChild(li);
+          icon.setAttribute('src', burger_menu_icon);
+          anchor.setAttribute('class', 'btn cms-moderation-action-btn closed');
+          anchor.setAttribute('title', 'Actions');
+          anchor.appendChild(icon);
 
-          /* destroy original replaced buttons */
-          actions[0].removeChild(item);
-        });
+          /* create options container */
+          let optionsContainer = document.createElement('div');
+          let ul = document.createElement('ul');
 
-        /* add the options to the drop-down */
-        optionsContainer.appendChild(ul);
-        actions[0].appendChild(anchor);
-        document.body.appendChild(optionsContainer);
+          /* 'cms-actions-dropdown-menu' class is the main selector for the menu,
+          'cms-actions-dropdown-menu-arrow-right-top' keeps the menu arrow in position. */
+          optionsContainer.setAttribute(
+            'class',
+            'cms-actions-dropdown-menu cms-actions-dropdown-menu-arrow-right-top');
+          ul.setAttribute('class', 'cms-actions-dropdown-menu-inner');
 
-        /* listen for burger menu clicks */
-        anchor.addEventListener('click', function (ev) {
-          ev.stopPropagation();
-          toggleBurgerMenu(anchor, optionsContainer);
-        });
+          /* get the existing actions and move them into the options container */
+          $(actions[0]).children('.cms-moderation-action-btn').each(function (index, item) {
 
-        /* close burger menu if clicking outside */
-        $(window).click(function () {
+            let li = document.createElement('li');
+            /* create an anchor from the item */
+            let li_anchor = document.createElement('a');
+
+            li_anchor.setAttribute('class', 'cms-actions-dropdown-menu-item-anchor');
+            li_anchor.setAttribute('href', $(item).attr('href'));
+
+            if ($(item).hasClass('cms-form-get-method')) {
+              li_anchor.classList.add('cms-form-get-method'); // Ensure the fake-form selector is propagated to the new anchor
+            }
+            /* get the span which contains the img */
+            let value = $(item).children('span')[0]
+            /* move the icon image */
+            li_anchor.appendChild($(value).children('img')[0]);
+
+            /* create the button text and construct the button */
+            let span = document.createElement('span');
+            span.appendChild(
+              document.createTextNode(item.title)
+            );
+
+            li_anchor.appendChild(span);
+            li.appendChild(li_anchor);
+            ul.appendChild(li);
+
+            /* destroy original replaced buttons */
+            actions[0].removeChild(item);
+          });
+
+          /* add the options to the drop-down */
+          optionsContainer.appendChild(ul);
+          actions[0].appendChild(anchor);
+          document.body.appendChild(optionsContainer);
+
+          /* listen for burger menu clicks */
+          anchor.addEventListener('click', function (ev) {
+            ev.stopPropagation();
+            toggleBurgerMenu(anchor, optionsContainer);
+          });
+
+          /* close burger menu if clicking outside */
+          $(window).click(function () {
+            closeBurgerMenu();
+          });
+        };
+
+        let toggleBurgerMenu = function toggleBurgerMenu(burgerMenuAnchor, optionsContainer) {
+          let bm = $(burgerMenuAnchor);
+          let op = $(optionsContainer);
+          let closed = bm.hasClass('closed');
           closeBurgerMenu();
+
+          if (closed) {
+            bm.removeClass('closed').addClass('open');
+            op.removeClass('closed').addClass('open');
+          } else {
+            bm.addClass('closed').removeClass('open');
+            op.addClass('closed').removeClass('open');
+          }
+
+          let pos = bm.offset();
+          op.css('left', pos.left - 200);
+          op.css('top', pos.top);
+        };
+
+        let closeBurgerMenu = function closeBurgerMenu() {
+          $('.cms-actions-dropdown-menu').removeClass('open');
+          $('.cms-actions-dropdown-menu').addClass('closed');
+          $('.cms-moderation-action-btn').removeClass('open');
+          $('.cms-moderation-action-btn').addClass('closed');
+        };
+
+        $('#result_list').find('tr').each(function (index, item) {
+          createBurgerMenu(item);
         });
-      };
-
-      let toggleBurgerMenu = function toggleBurgerMenu(burgerMenuAnchor, optionsContainer) {
-        let bm = $(burgerMenuAnchor);
-        let op = $(optionsContainer);
-        let closed = bm.hasClass('closed');
-        closeBurgerMenu();
-
-        if (closed) {
-          bm.removeClass('closed').addClass('open');
-          op.removeClass('closed').addClass('open');
-        } else {
-          bm.addClass('closed').removeClass('open');
-          op.addClass('closed').removeClass('open');
-        }
-
-        let pos = bm.offset();
-        op.css('left', pos.left - 200);
-        op.css('top', pos.top);
-      };
-
-      let closeBurgerMenu = function closeBurgerMenu() {
-        $('.cms-actions-dropdown-menu').removeClass('open');
-        $('.cms-actions-dropdown-menu').addClass('closed');
-        $('.cms-moderation-action-btn').removeClass('open');
-        $('.cms-moderation-action-btn').addClass('closed');
-      };
-
-      $('#result_list').find('tr').each(function (index, item) {
-        createBurgerMenu(item);
       });
-    });
-    
 })((typeof django !== 'undefined' && django.jQuery) || (typeof CMS !== 'undefined' && CMS.$) || false);
