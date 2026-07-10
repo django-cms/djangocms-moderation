fc1d7b57d8924dad022ca3893b2ba5b749eebd76
Fabian Braun <fsbraun@gmx.de>
2026-07-10 21:36:50 +0200

    tests: Failed due to stale cache (#378)
    * tests: Failed due to stale cache
    
    * fix: Clear cache earler

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -20,14 +20,13 @@ class VersionLockingTestCase(BaseTestCase):
         self.assertTrue(is_obj_version_unlocked(version.content, self.user))
         self.assertFalse(is_obj_version_unlocked(version.content, self.user2))
         version.publish(self.user)
-        # reload version to update cache
-        version = Version.objects.get_for_content(version.content)
-        self.assertTrue(is_obj_version_unlocked(version.content, self.user2))
+        content = type(version.content)._base_manager.get(pk=version.content.pk)
+        self.assertTrue(is_obj_version_unlocked(content, self.user2))
 
         # Make sure that we are actually calling the version-lock method and it
         # still exists
         with mock.patch(
             "djangocms_moderation.helpers.content_is_unlocked_for_user"
         ) as _mock:
-            is_obj_version_unlocked(version.content, self.user2)
-            _mock.assert_called_once_with(version.content, self.user2)
+            is_obj_version_unlocked(content, self.user2)
+            _mock.assert_called_once_with(content, self.user2)
