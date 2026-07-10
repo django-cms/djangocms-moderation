6a43efbe8956a95587c02d9a7826ae8a86eb207e
Fabian Braun <fsbraun@gmx.de>
2026-07-10 21:34:51 +0200

    fix: Clear cache earler

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -20,11 +20,7 @@ class VersionLockingTestCase(BaseTestCase):
         self.assertTrue(is_obj_version_unlocked(version.content, self.user))
         self.assertFalse(is_obj_version_unlocked(version.content, self.user2))
         version.publish(self.user)
-        # reload version to update cache
-        version = Version.objects.get_for_content(version.content)
-        content = version.content
-        if hasattr(content, "prefetched_versions"):
-            del content.prefetched_versions
+        content = type(version.content)._base_manager.get(pk=version.content.pk)
         self.assertTrue(is_obj_version_unlocked(content, self.user2))
 
         # Make sure that we are actually calling the version-lock method and it
